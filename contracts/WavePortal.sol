201fc3de119032842d687e110a69f7dc88a50e25
tselouie <tselouie@gmail.com>
2021-11-25 12:38:55 -0500

    console log contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "../node_modules/hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+}
