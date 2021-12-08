1835eb59675655e00474183b9655efd982b74dea
tselouie <tselouie@gmail.com>
2021-12-08 14:52:40 -0500

    add rinkeby to networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -24,6 +26,12 @@ module.exports = {
         enabled: true,
         runs: 200
       }
+    }
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     }
   },
   paths: {
